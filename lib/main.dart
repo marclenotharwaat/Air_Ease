31268f0603a1b404ef225b9c3507b81f15667215
marclenotharwaat <marclenotharwat@gmail.com>
2024-05-12 22:28:35 +0300

    add flight widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/intro/intro_screen.dart';
 
 void main() {
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,6 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+            theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromRGBO(33, 134, 140, 1),
+        appBarTheme: AppBarTheme(
+          color: Color.fromRGBO(33, 134, 140, 1),
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme()
+      ),
       debugShowCheckedModeBanner: false,
       home: IntroScreen() // Use the correct method name here
     );
---
+++ lib/widget/flight.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+
+class Flight extends StatelessWidget {
+  const Flight(
+      {super.key,
+      required this.fromCity,
+      required this.toCity,
+      required this.date,
+      required this.timeOfFlight,
+      required this.flightNumber,
+      required this.price,
+      required this.fromCountry,
+      required this.toCountry});
+  final String fromCity;
+  final String fromCountry;
+  final String toCity;
+  final String toCountry;
+  final String date;
+  final String timeOfFlight;
+  final String flightNumber;
+  final double price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 175,
+            padding: const EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      '$fromCountry',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Colors.indigo.shade50,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: SizedBox(
+                        height: 8,
+                        width: 8,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(
+                                color: Colors.indigo.shade400,
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                          children: [
+                            Stack(
+                              children: [
+                                SizedBox(
+                                  height: 24,
+                                  child: LayoutBuilder(
+                                    builder: (context, constraints) {
+                                      return Flex(
+                                        direction: Axis.horizontal,
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .spaceBetween, // or Axis.vertical,
+                                        children: List.generate(
+                                          (constraints.constrainWidth() / 6)
+                                              .floor(),
+                                          (index) => const SizedBox(
+                                            height: 1,
+                                            width: 3,
+                                            child: DecoratedBox(
+                                              decoration: BoxDecoration(
+                                                color: Color.fromRGBO(
+                                                    33, 134, 140, 1),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                Center(
+                                    child: Transform.rotate(
+                                  angle: 1.5,
+                                  child: Icon(
+                                    Icons.local_airport,
+                                    color: Colors.indigo.shade300,
+                                    size: 24,
+                                  ),
+                                ))
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Colors.indigo.shade50,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: SizedBox(
+                        height: 8,
+                        width: 8,
+                        child: DecoratedBox(
+                            decoration: BoxDecoration(
+                                color: Colors.pink.shade400,
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Text(
+                      '$toCountry',
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 100,
+                      child: Text(
+                        '$fromCity',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    Text(
+                      '$timeOfFlight',
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: Text(
+                        '$toCity',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(fontSize: 14, color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Date & Time',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Flight Number',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      '$date',
+                      style: TextStyle(fontSize: 14, color: Colors.black),
+                    ),
+                    Text(
+                      '$flightNumber',
+                      style: TextStyle(fontSize: 14, color: Colors.black),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        color: Color.fromRGBO(33, 134, 140, 1)),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      children: [
+                        LayoutBuilder(
+                          builder: (context, constraints) {
+                            return Flex(
+                              direction: Axis.horizontal,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment
+                                  .spaceBetween, // or Axis.vertical,
+                              children: List.generate(
+                                (constraints.constrainWidth() / 10).floor(),
+                                (index) => const SizedBox(
+                                  height: 1,
+                                  width: 5,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(33, 134, 140, 1),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                  width: 10,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10)),
+                        color: Color.fromRGBO(33, 134, 140, 1)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 50,
+            padding: EdgeInsets.only(left: 16, right: 16, bottom: 5),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20)),
+            ),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.amber.shade50,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Container(
+                      height: 31.77,
+                      width: 50,
+                      child: Image.asset(
+                          "assets/large-NaZxbgjtn--removebg-preview-removebg-preview.png")),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Text(
+                    "\$500",
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
